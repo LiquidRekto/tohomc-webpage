8b83c9b510c78f404cec16626b7f94c5609da285
LiquidRekto <dungliqrekt@gmail.com>
2023-08-01 00:58:10 +0700

    Adding several contents to the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        glow: ["0 0px 20px rgba(100,100, 255, 0.5)", "0 0px 65px rgba(100, 100,255, 0.4)"],
+        wglow: ["0 0px 20px rgba(255,255, 255, 0.35)", "0 0px 65px rgba(255, 255,255, 0.2)"],
+      }
+    },
   },
   plugins: [],
 }
